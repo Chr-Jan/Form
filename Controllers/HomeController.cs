1914d7731bec7782d56601a3a998c4cef98b011b
Christoffer Jansson <christofferjansson1@gmail.com>
2024-01-12 08:37:34 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Form.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Form.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(Human human)
+        {
+            string name = human.Name;
+
+            int age = human.Age;
+
+            bool handsome = human.Handsome;
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Human.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Form.Models
+{
+    public class Human
+    {
+        [Required]
+        [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Name must contain letters only")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "Age must contain numbers only")]
+        [Range(0, int.MaxValue)]
+        public int Age { get; set; }
+
+        public bool Handsome { get; set; }
+    }
+}
